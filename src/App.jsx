adab080eede7497a058a6551292fff8c4b6cf172
Dimon1989k <106411942+Dimon1989k@users.noreply.github.com>
2023-11-15 22:56:06 +0200

    add store, slice, form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import PostList from "./components/PostList";
+import AddFormPost from "./components/addFormPost";
+
+function App() {
+  return (
+    <div>
+      <PostList />
+      <AddFormPost />
+    </div>
+  );
+}
+
+export default App;
